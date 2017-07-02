d60fb60d97c52237f99b936d9f5d31f79cacec6f
yl91 <yangli_sky@163.com>
2017-07-02 16:08:48 +0800

    'add'

---
+++ src/main/java/com/qunar/TestController.java
@@ -0,0 +1,12 @@
+package com.qunar;
+
+import sun.applet.Main;
+
+/**
+ * Created by Administrator on 2017/7/2.
+ */
+public class TestController {
+    public void  test(){
+        System.out.println("123");
+    }
+}
